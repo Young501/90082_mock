245e96bdcff4dd00842bc188c08c8fffc53d87a6
haoyeye5 <89088982+haoyeye5@users.noreply.github.com>
2025-05-30 13:48:34 +1000

    create AuthContext, useAuth() hook, wrap app with AuthProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,37 @@
-'use client';
+// 'use client';
 
-import { ReactNode } from 'react';
-import Providers from '@/components/ui/providers';
+// import { ReactNode } from 'react';
+// import Providers from '@/components/ui/providers';
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+// export default function RootLayout({ children }: { children: ReactNode }) {
+//   return (
+//     <html lang="en" suppressHydrationWarning>
+//       <body>
+//         <Providers>
+//           {children}
+//         </Providers>
+//       </body>
+//     </html>
+//   );
+// }
+
+
+// app/layout.tsx
+import './globals.css';
+import {AuthProvider} from '@/context/AuthContext';
+
+export const metadata = {
+  title: 'UniConnected2',
+  description: 'This is UniConnected 2.0',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body>
-        <Providers>
-          {children}
-        </Providers>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
 }
+
