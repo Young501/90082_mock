9c8a490e43106464cea82880cd9c2a3260d8603e
marlstergo <abdulmaliqshola@gmail.com>
2025-06-16 13:55:18 +0100

    update protected route on page refresh

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,22 +1,35 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { useAuth } from '@/api';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/api";
 
-export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
+export default function ProtectedRoute({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const router = useRouter();
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, token } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    if (!isAuthenticated) {
-      router.push('/login');
-    }
-  }, [isAuthenticated, router]);
+    const checkAuth = async () => {
+      if (!isLoading && !isAuthenticated && !token) {
+        router.push("/login");
+      }
+      setIsLoading(false);
+    };
+    checkAuth();
+  }, [isAuthenticated, token, isLoading, router]);
 
-  if (!isAuthenticated) {
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated || !token) {
     return null;
   }
 
   return <>{children}</>;
-} 
+}
