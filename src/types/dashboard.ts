187680607fd2bd69fbaeb7b5f942017f07f07cbf
marlstergo <developer@abdulmaliq..dev>
2025-07-18 07:55:45 +0100

    updates

--- src/types/dashboard.ts
+++ src/types/dashboard.ts
@@ -12,3 +12,56 @@ export interface DashboardStats {
     matched: number;
   };
 }
+
+export interface Participant {
+  id: number;
+  name?: string;
+  email?: string;
+  user_type?: string;
+  accepted_status?: string;
+  has_profile?: boolean;
+  match_info?: {
+      is_matched: boolean;
+      matched_with: {
+          id: number;
+          name?: string;
+          email?: string;
+          user_type?: string;
+      }
+  },
+  messages?: [
+              {
+                  id: number;
+                  sender: {
+                      id: number;
+                      name?: string;
+                      email?: string;
+                      user_type?: string;
+                  },
+                  receiver: {
+                      id: number;
+                      name?: string;
+                      email?: string;
+                      user_type?: string;
+                  },
+                      sent_at: string;
+              }
+          ]
+}
+
+
+export interface ParticipantsResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Participant[];
+}
+
+export interface ParticipantsFilterParams {
+  user_type?: string;
+  text?: string;
+  accepted_status?: string;
+  matched?: string;
+  page?: number;
+  page_size?: number;
+}
