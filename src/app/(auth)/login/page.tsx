d4feb8461b286bd684e36da81505de0551ec4aa1
marlstergo <developer@abdulmaliq..dev>
2025-07-22 13:53:04 +0100

    fix call stack redirect issue

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -56,7 +56,7 @@ export default function LoginPage() {
         password: data.password,
         callback: () => {
           toast.success("Login successful!");
-          router.push("/discover/");
+          // router.push("/discover/");
         },
       });
     } catch (error: any) {
