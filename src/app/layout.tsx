fb5d1648b67f172fc4b9f395b01f24b34123803c
marlstergo <developer@abdulmaliq..dev>
2025-08-07 09:47:28 +0100

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,27 @@
-"use client";
 
 import { ReactNode } from "react";
 import Providers from "@/components/ui/providers";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Unniconnected',
+    template: '%s | Unniconnected'
+  },
+  description: 'Unniconnected',
+  icons: {
+    icon: '/favicons/favicon.ico',
+    shortcut: '/favicons/favicon.ico',
+    apple: '/favicons/apple-touch-icon.png',
+    other: {
+      rel: 'apple-touch-icon',
+      url: '/favicons/apple-touch-icon.png',
+    },
+  },
+  manifest: '/favicons/site.webmanifest',
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
