197d74055404ffa21630dc9d4f1683fda2180062
marlstergo <developer@abdulmaliq..dev>
2025-08-07 10:42:12 +0100

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <head>
         <link rel="icon" href="/favicons/favicon.ico" />
         <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32" />
         <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
         <link rel="manifest" href="/favicons/site.webmanifest" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
