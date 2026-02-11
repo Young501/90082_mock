96d832469dd7a0f9294a1c497cdf89a25603ca8c
marlstergo <developer@abdulmaliq.dev>
2026-02-11 16:10:11 +0100

    updates

---
+++ src/components/Icons/SuccessBubbles.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+
+const SuccessBubbles = () => {
+  return (
+    <svg
+      width="171"
+      height="150"
+      viewBox="0 0 171 150"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M143.539 31.3085C148.476 31.7885 158.802 30.1978 160.612 19.9949C162.423 9.79197 167.264 6.96697 169.458 6.82983"
+        stroke="#DC2626"
+        strokeWidth="3"
+        strokeLinecap="round"
+      />
+      <circle cx="100.127" cy="72.8735" r="49.3594" fill="#16A34A" />
+      <g filter="url(#filter0_d_888_30894)">
+        <path
+          d="M99.5234 31.2327C122.807 31.2327 141.682 50.1075 141.682 73.3909C141.682 96.6742 122.807 115.549 99.5234 115.549C76.2401 115.549 57.3652 96.6742 57.3652 73.3909C57.3653 50.1075 76.2401 31.2327 99.5234 31.2327Z"
+          stroke="white"
+          strokeOpacity="0.7"
+          strokeWidth="2"
+        />
+      </g>
+      <path
+        d="M88.3535 71.1875L81.2285 91.25L101.291 84.1437M84.9785 55.625H84.9973M118.729 65H118.747M105.604 53.75H105.622M118.729 87.5H118.747M118.729 53.75L114.529 55.1562C113.333 55.5545 112.313 56.3556 111.643 57.4226C110.972 58.4896 110.693 59.7564 110.854 61.0062C111.041 62.6187 109.785 64.0625 108.135 64.0625H107.422C105.81 64.0625 104.422 65.1875 104.122 66.7625L103.729 68.75M118.729 74.375L117.191 73.7563C115.579 73.1188 113.779 74.1312 113.479 75.8375C113.272 77.15 112.129 78.125 110.797 78.125H109.354M98.1035 53.75L98.7223 55.2875C99.3598 56.9 98.3473 58.7 96.641 59C95.3285 59.1875 94.3535 60.35 94.3535 61.6813V63.125M98.1035 74.375C101.722 77.9937 103.41 82.1938 101.854 83.75C100.297 85.3062 96.0973 83.6187 92.4785 80C88.8598 76.3813 87.1723 72.1812 88.7285 70.625C90.2848 69.0688 94.4848 70.7563 98.1035 74.375Z"
+        stroke="white"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M136.656 122.285C141.264 123.617 150.173 128.675 148.944 138.259"
+        stroke="#EA580C"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1.5 108.76C8.57112 111.423 23.8154 113.333 28.2234 99.6687M28.2234 99.6687C28.8663 95.9036 28.2235 88.4559 20.5096 88.7865C19.0402 92.4139 18.5258 99.6687 28.2234 99.6687ZM28.2234 99.6687C31.4376 100.266 38.9128 99.6963 43.1004 92.6435"
+        stroke="#2563EB"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <circle cx="113.714" cy="145.269" r="4.73121" fill="#2563EB" />
+      <circle cx="158.44" cy="70.3474" r="4.23121" stroke="#DB2777" />
+      <ellipse
+        cx="83.3373"
+        cy="5.18107"
+        rx="5.18107"
+        ry="5.18107"
+        fill="#9333EA"
+      />
+      <circle cx="98.0467" cy="13.9261" r="2.73617" fill="#DC2626" />
+      <circle cx="37.7404" cy="54.1088" r="4.2951" stroke="#EA580C" />
+      <circle cx="48.755" cy="129.719" r="4.77259" stroke="#9333EA" />
+      <circle
+        cx="2.51892"
+        cy="2.51892"
+        r="2.51892"
+        transform="matrix(-1 0 0 1 101.521 137.324)"
+        fill="#9333EA"
+      />
+      <defs>
+        <filter
+          id="filter0_d_888_30894"
+          x="52.3652"
+          y="29.2327"
+          width="94.3164"
+          height="94.3164"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="3" />
+          <feGaussianBlur stdDeviation="2" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_888_30894"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_888_30894"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
+
+export default SuccessBubbles;
