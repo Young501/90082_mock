bb11afbe1b2b43f42fb4b992b7d0c442e9504b41
Zechen Yang <yangzechenau@gmail.com>
2026-08-25 21:00:52 +1000

    Open prototype page by default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,13 @@
 "use client";
-import { redirect } from "next/navigation";
-import { useAuthStore } from "@/store";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const { user, isAuthenticated, token } = useAuthStore();
   const router = useRouter();
 
   useEffect(() => {
-    if (isAuthenticated && token && user) {
-      const userType = user.user_types?.[0];
-      if (userType === "coordinator") {
-        router.push("/dashboard/");
-      } else {
-        router.push("/home/");
-      }
-    } else {
-      router.push("/user-type/");
-    }
-  }, [isAuthenticated, token, user, router]);
+    router.replace("/prototype/");
+  }, [router]);
 
   return null;
 }
